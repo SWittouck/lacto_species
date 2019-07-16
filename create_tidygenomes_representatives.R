99861adca0e53aa5863891c397c62310344bb088
Stijn Wittouck <stijn.wittouck@hotmail.com>
2019-07-16 16:18:53 +0200

    Adapt to creation of short genus names in earlier script

--- create_tidygenomes_representatives.R
+++ create_tidygenomes_representatives.R
@@ -11,12 +11,6 @@ clusters_species <- read_csv("parsed_v3/clusters_all_named.csv")
 phylogroups <- read_csv("parsed_v3/phylogroups.csv")
 pairs <- read_csv("input_v3/genome_pairs.csv.zip")
 
-clusters_species <- 
-  clusters_species %>%
-  mutate(species_full = species) %>%
-  mutate_at("species", str_replace, "Leuconostoc", "Leuc\\.") %>%
-  mutate_at("species", str_replace, "(?<=[A-Z])[a-z]{5,}", "\\.")
-
 phylogroups <-
   phylogroups %>%
   rename(genome_type = species_type, genome_peripheral = species_peripheral)
@@ -28,7 +22,7 @@ lgc_repr <-
   add_tidygenomes(tree) %>%
   add_genome_metadata(genomes_clusters) %>%
   add_genome_metadata(clusters_species, by = "cluster") %>%
-  add_phylogroups(phylogroups, genome_identifier = species) %>%
+  add_phylogroups(phylogroups, genome_identifier = species_short) %>%
   add_tidygenomes(pairs)
 
 save(lgc_repr, file = "parsed_v3/lgc_tidygenomes_representatives.rda")
